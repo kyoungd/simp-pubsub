de9a08b9cf03518a4c57997af121efc1fabc534d
kyoungd <kyoungd@hotmail.com>
2022-12-02 10:50:48 -0800

    added single instance pattern

--- utilGetJwt.js
+++ utilGetJwt.js
@@ -1,8 +1,10 @@
 import axios from 'axios';;
 
 class GetJwt {
+    jwt = null;
+
     constructor() {
-        this.jwt = null;
+        GetJwt.jwt = null;
         this.login = process.env.URL_JWT_USERNAME || 'syop01';
         this.password = process.env.URL_JWT_PASSWORD || 'password';
         this.url = process.env.URL_JWT_LOGIN || 'http://localhost:1337/api/auth/local';
@@ -13,16 +15,17 @@ class GetJwt {
             const response = await axios.post(url, {identifier: login, password: password});
             console.log('User profile', response.data.user);
             console.log('User token', response.data.jwt);
-            this.jwt = response.data.jwt;
+            GetJwt.jwt = response.data.jwt;
         }
         catch(error){
             console.log('An error occurred:', error.response);
-            this.jwt = null;
+            GetJwt.jwt = null;
         };
-        return this.jwt;    
+        return GetJwt.jwt;    
     }  
 
     static async run() {
+        if (GetJwt.jwt) return GetJwt.jwt;
         const jwt = new GetJwt();
         return await jwt.getJwt(jwt.url, jwt.login, jwt.password);
     }
